05f4d9185373985b9660f8f3ee4b0658cef6a95e
Charles Drake <cdrake1205@gmail.com>
2024-04-21 10:03:53 -0400

    changed js for scroll effect

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ function Pasta() {
   )
 }
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+document.querySelectorAll('a[href*="#"]').forEach(anchor =>{
   anchor.addEventListener("click", function(e){
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
